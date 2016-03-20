76f28ed55e0a8239f7920708cf505e70d1d94ebf
DanielBonnery <daniel.bonnery@gmail.com>
2016-03-19 23:48:54 -0400

    a

--- demo/casen2009Regional.R
+++ demo/casen2009Regional.R
@@ -1,7 +1,7 @@
 data(casen2009)
 library(survey)
 householdlevel<-unique(casen2009[c("ESTRATO",
-"SEGMENTO","EXPR","FOLIO","CORTE","REGION","COMUNA")])
+"SEGMENTO","EXPR","FOLIO","CORTE","REGION","COMUNA","NUMPER")])
 #remove duplicates
 counts<-table(householdlevel$FOLIO)
 toremove<-names(counts[counts>1])
@@ -22,3 +22,7 @@ library(ggplot2)
 plot1<-ggplot(povertyrateperregion, aes(x = REGION, y = POBRETRUE)) +
   geom_point(size = 4) +
   geom_errorbar(aes(ymax = U, ymin = L))
+attach(householdlevel)
+sapply(levels(REGION),function(x){
+  sum(EXPR*POBRE*NUMPER*(REGION==x))/sum(EXPR*NUMPER*(REGION==x))
+})
