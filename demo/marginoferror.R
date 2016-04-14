d31e97c0d4b67791f5a2331688f8679d76516681
DanielBonnery <daniel.bonnery@gmail.com>
2016-04-14 11:32:32 -0400

    a

--- demo/marginoferror.R
+++ demo/marginoferror.R
@@ -3,6 +3,6 @@ n<-c(10,100,1000)
 p=rep(seq(0,1,length.out=npoints),length(n))
 n<-rep(n,each=npoints)
 library(ggplot2)
-plot1<-ggplot(data = data.frame(p,n,y=qnorm(0.975)*sqrt(p*(1-p)/n),Size=n),
+plot1<-ggplot(data = data.frame(p,n,y=qnorm(0.975)*sqrt(p*(1-p)/n),Size=factor(n)),
               aes(x=p, y=y, group=Size, colour = Size))+
   geom_line()
